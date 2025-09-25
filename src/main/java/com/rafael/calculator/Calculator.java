01bc086302916485a6d0afaadde02d3a122f9056
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-09-25 10:25:45 +0200

    initial commit

---
+++ src/main/java/com/rafael/calculator/Calculator.java
@@ -0,0 +1,4 @@
+package com.rafael.calculator;
+
+public class Calculator {
+}
---
+++ src/test/java/com/rafael/calculator/CalculatorTest.java
@@ -0,0 +1,7 @@
+package com.rafael.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+}
