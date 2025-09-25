bd60877fc65ec04354bd72847520aa117c9616e4
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-09-25 10:43:57 +0200

    premiers tests

--- src/main/java/com/rafael/calculator/Calculator.java
+++ src/main/java/com/rafael/calculator/Calculator.java
@@ -1,4 +1,11 @@
 package com.rafael.calculator;
 
 public class Calculator {
+    public int add(int opG, int opD){
+        return opG + opD;
+    }
+
+    public int divide(int opG, int opD){
+        return opG / opD;
+    }
 }
--- src/test/java/com/rafael/calculator/CalculatorTest.java
+++ src/test/java/com/rafael/calculator/CalculatorTest.java
@@ -1,7 +1,50 @@
 package com.rafael.calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void devrait_renvoyer_additionner_les_deux_nombres() {
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 2;
+        int opD = 3;
 
+        // WHEN
+        int res = calculator.add(opG,opD);
+
+        // THEN
+        assertThat(res).isEqualTo(5);
+    }
+
+    @Test
+    void devrait_renvoyer_soustraire_les_deux_nombres() {
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 2;
+        int opD = -3;
+
+        // WHEN
+        int res = calculator.add(opG,opD);
+
+        // THEN
+        assertThat(res).isEqualTo(-1);
+    }
+
+    @Test
+    void devrait_renvoyer_diviser_les_deux_nombres() {
+        // GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 4;
+        int opD = 2;
+
+        // WHEN
+        int res = calculator.divide(opG,opD);
+
+        // THEN
+        assertThat(res).isEqualTo(2);
+    }
 }
