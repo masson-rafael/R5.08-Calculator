8f9dc5519047bf4c2dd480cb7e5bf36cd3c409d2
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-09-25 11:28:56 +0200

    test paramétré

--- src/test/java/com/rafael/calculator/CalculatorTest.java
+++ src/test/java/com/rafael/calculator/CalculatorTest.java
@@ -2,11 +2,30 @@ package com.rafael.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @ParameterizedTest(name = "Test {index}: {0} + {1} = {2}")
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void devrait_renvoyer_le_resultat_de_toutes_les_additions(int opG, int opD, int resultatAttendu) {
+        // WHEN
+        int res = Calculator.add(opG, opD);
+
+        // THEN
+        assertThat(res).isEqualTo(resultatAttendu);
+    }
 
     @Test
     void devrait_renvoyer_additionner_les_deux_nombres() {
